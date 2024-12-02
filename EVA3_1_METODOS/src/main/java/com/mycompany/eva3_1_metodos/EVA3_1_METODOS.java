af7b82bb64aa10ecf7cf4706a0b26aa07cbb083d
JLOPVL33 <L24550321@chihuahua2.tecnm.mx>
2024-12-02 00:27:22 -0600

    Add files via upload

---
+++ EVA3_1_METODOS/src/main/java/com/mycompany/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_1_metodos;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EVA3_1_METODOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //Uso de metodos 
+        imprimirMensaje(" Hola mundo!!");
+    }
+    //Encabezado
+    //Imprimir un mensaje 
+    //JAVA; Iniciar en minusculas, verbo,siguientes palabras iniciar
+    public static void imprimirMensaje(String mensaje){
+        System.out.println("Tu mensaje" + mensaje);
+    }
+}
